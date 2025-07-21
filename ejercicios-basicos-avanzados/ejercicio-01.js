97c2cd8db9f50122ec1fa787c33e2470ae846d8c
JaumeAdell <jaumeadell2000@gmail.com>
2025-07-21 18:10:14 +0200

    ejercicio 1 y seteo node;

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,14 @@
+let miSuperheroeFavorito = "Hulk";
+console.log(miSuperheroeFavorito);
+
+let numeroFavorito = 50;
+console.log(numeroFavorito);
+
+let altura = 5;
+let longitud = 10;
+console.log(altura);
+console.log(longitud);
+
+
+let suma = altura + longitud;
+console.log(suma);
