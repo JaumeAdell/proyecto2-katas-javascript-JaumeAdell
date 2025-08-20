34ebef1c81c0df78af2879cd8e742b5a88a980e6
JaumeAdell <jaumeadell2000@gmail.com>
2025-08-20 20:00:38 +0200

    mas ejercicios proyecto katas

---
+++ ejercicios-basicos-avanzados/ejercicio-07.js
@@ -0,0 +1,7 @@
+function greaterNumber(numberOne , numberTwo) {
+  if(numberOne > numberTwo){
+    console.log(numberOne);
+  } else if (numberTwo > numberOne){
+    console.log(numberTwo);
+    } 
+  }
