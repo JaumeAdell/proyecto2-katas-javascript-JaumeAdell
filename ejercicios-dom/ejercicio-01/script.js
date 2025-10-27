70d0577bea021efdd76d5d65b0cbcb24ccf316bf
JaumeAdell <jaumeadell2000@gmail.com>
2025-10-27 06:33:53 +0100

    cambios proyecto dom

---
+++ ejercicios-dom/ejercicio-01/script.js
@@ -0,0 +1,17 @@
+const boton = document.querySelector(".showme");
+console.log(boton);
+
+const h1 = document.querySelector("#pillado");
+console.log(h1);
+
+const parrafos = document.querySelectorAll("p");
+console.log(parrafos);
+
+const pokemons = document.querySelectorAll(".pokemon");
+console.log(pokemons);
+
+const personajes = document.querySelectorAll('[data-function="testMe"]');
+console.log(personajes);
+
+const tercerPersonaje = personajes[2];
+console.log(tercerPersonaje);
